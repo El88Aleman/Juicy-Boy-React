b3941caa020fcbff731321b0ea7e6c706b4dbc49
El88Aleman <113607297+El88Aleman@users.noreply.github.com>
2023-06-29 10:50:53 -0300

    ultimo commit antes de esta rama

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import Navbar from "./components/layout/navBar/Navbar";
 import Footer from "./components/layout/footer/Footer";
-import { useState } from "react";
 import ItemListContainer from "./pages/itemListContainer/ItemListContainer";
 import CounterContainer from "./components/common/counter/CounterContainer";
+
 function App() {
-  const [mostrarComponente, setMostrarComponente] = useState(false);
-
-  const [numero, setNumero] = useState(0);
-
   return (
     <div>
       <Navbar />
       <Footer />
-      <button onClick={() => setNumero(numero + 1)}>Sumar Numero</button>
-      <button onClick={() => setMostrarComponente(!mostrarComponente)}>
-        Montar/Desmontar
-      </button>
-      {mostrarComponente && <ItemListContainer />}
+      <ItemListContainer />
       <CounterContainer />
     </div>
   );
