27cb8dd978cf7801952b8b04151dbaf8ce457c61
El88Aleman <113607297+El88Aleman@users.noreply.github.com>
2023-12-12 06:42:09 -0300

    preguntar dudas

--- src/components/common/productCard/ProductCard.jsx
+++ src/components/common/productCard/ProductCard.jsx
@@ -6,12 +6,63 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
+import "./ProductCard.css";
+import { useState } from "react";
 
 const ProductCard = ({ item }) => {
+  const [sliderImage, setSliderImage] = useState(0);
+
+  const siguienteImagen = () => {
+    setSliderImage(sliderImage === item.img - 1 ? 0 : sliderImage + 1);
+  };
+
+  const anteriorImagen = () => {
+    setSliderImage(sliderImage === 0 ? item.img - 1 : sliderImage - 1);
+  };
+
   return (
     <Card style={{ textAlign: "center" }} sx={{ width: 300 }}>
-      <CardMedia sx={{ height: 500 }} image={item.img} title="Juicy-Boy" />
+      <div style={{ position: "relative", width: "100%" }}>
+        <CardMedia sx={{ height: 500 }} image={item.img} title="Juicy-Boy" />
+        <div>
+          <span
+            className="back"
+            style={{
+              color: "white",
+              position: "absolute",
+              top: "50%",
+              left: "2rem",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <IoIosArrowBack
+              onClick={anteriorImagen}
+              className="icon"
+              size={60}
+            />
+          </span>
+          <span
+            className="next"
+            style={{
+              color: "white",
+              position: "absolute",
+              top: "50%",
+              right: "2rem",
+              transform: "translate(50%, -50%)",
+            }}
+          >
+            <IoIosArrowForward
+              onClick={siguienteImagen}
+              className="icon"
+              size={60}
+            />
+          </span>
+        </div>
+      </div>
+
       <CardContent>
         <Typography
           style={{ fontFamily: "bitwise", height: "10vh" }}
--- src/productsMock.js
+++ src/productsMock.js
@@ -6,16 +6,8 @@ export const products = [
     description: "Estilo Oversize",
     category: "Negro",
     img: [
-      {
-        id: 1,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700161869/cowbhs8qct7rjoew10ec.jpg",
-        alt: "imagen1",
-      },
-      {
-        id: 2,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700599438/m8b3qtampuvsa8xbmwo5.jpg",
-        alt: "imagen2",
-      },
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700161869/cowbhs8qct7rjoew10ec.jpg",
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700599438/m8b3qtampuvsa8xbmwo5.jpg",
     ],
   },
   {
@@ -25,16 +17,8 @@ export const products = [
     description: "Estilo Oversize",
     category: "Negro",
     img: [
-      {
-        id: 1,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159139/pqezyuhceskzbdvtqt2s.jpg",
-        alt: "imagen 1",
-      },
-      {
-        id: 2,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700597314/ahkmmpwiqoyxqfynfhtl.jpg",
-        alt: "imagen 2",
-      },
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159139/pqezyuhceskzbdvtqt2s.jpg",
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700597314/ahkmmpwiqoyxqfynfhtl.jpg",
     ],
   },
   {
@@ -44,16 +28,8 @@ export const products = [
     description: "Estilo Oversize",
     category: "Negro",
     img: [
-      {
-        id: 1,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159179/rmyfylgpamkcqgbpwe8n.jpg",
-        alt: "imagen1",
-      },
-      {
-        id: 2,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700600182/ftpmhg0lthlbxjpuxx5s.jpg",
-        alt: "imagen2",
-      },
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159179/rmyfylgpamkcqgbpwe8n.jpg",
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700600182/ftpmhg0lthlbxjpuxx5s.jpg",
     ],
   },
   {
@@ -63,16 +39,8 @@ export const products = [
     description: "Estilo Oversize",
     category: "Negro",
     img: [
-      {
-        id: 1,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159122/d5ps2s3ftwyhwwpsnxea.jpg",
-        alt: "imagen1",
-      },
-      {
-        id: 2,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700600400/mtssgcccgnp2rkybepdt.jpg",
-        alt: "imagen2",
-      },
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159122/d5ps2s3ftwyhwwpsnxea.jpg",
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700600400/mtssgcccgnp2rkybepdt.jpg",
     ],
   },
   {
@@ -82,16 +50,8 @@ export const products = [
     description: "Estilo Oversize",
     category: "Negro",
     img: [
-      {
-        id: 1,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159081/hbr3l4p7ajy2jh7mignc.jpg",
-        alt: "imagen1",
-      },
-      {
-        id: 2,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700600555/rl2vb8eeby7z0wfcncax.jpg",
-        alt: "imagen2",
-      },
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159081/hbr3l4p7ajy2jh7mignc.jpg",
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700600555/rl2vb8eeby7z0wfcncax.jpg",
     ],
   },
   {
@@ -101,16 +61,8 @@ export const products = [
     description: "Estilo Oversize",
     category: "Negro",
     img: [
-      {
-        id: 1,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159216/vdno0s7emlcckvzkkkyk.jpg",
-        alt: "imagen1",
-      },
-      {
-        id: 2,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700600830/yblllhylndntjy0mmicq.jpg",
-        alt: "imagen2",
-      },
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159216/vdno0s7emlcckvzkkkyk.jpg",
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700600830/yblllhylndntjy0mmicq.jpg",
     ],
   },
   {
@@ -120,16 +72,8 @@ export const products = [
     description: "Estilo Oversize",
     category: "Blanco",
     img: [
-      {
-        id: 1,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159234/ydxg24l4jyopx5za3odo.jpg",
-        alt: "imagen1",
-      },
-      {
-        id: 2,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700601011/xynuwhdzuba8jgerk6me.jpg",
-        alt: "imagen2",
-      },
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159234/ydxg24l4jyopx5za3odo.jpg",
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700601011/xynuwhdzuba8jgerk6me.jpg",
     ],
   },
   {
@@ -139,16 +83,8 @@ export const products = [
     description: "Estilo Oversize",
     category: "Blanco",
     img: [
-      {
-        id: 1,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159022/imuej2d2mprq8i50p8v5.jpg",
-        alt: "imagen1",
-      },
-      {
-        id: 2,
-        src: "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700601320/tatekn5sygrjgrkhqpa0.jpg",
-        alt: "imagen2",
-      },
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700159022/imuej2d2mprq8i50p8v5.jpg",
+      "https://res.cloudinary.com/dfcnmxndf/image/upload/v1700601320/tatekn5sygrjgrkhqpa0.jpg",
     ],
   },
 ];
